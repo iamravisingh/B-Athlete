e78d256c9d9f76ac32f3e985731cc86dace1fea9
Ravi Singh <ravi.singh@adapty.com>
2020-05-26 01:12:27 +0530

    change for deploying

--- src/component/menu-item/menu-item.component.jsx
+++ src/component/menu-item/menu-item.component.jsx
@@ -11,7 +11,7 @@ const MenuItem = ({title,imageUrl,history,linkUrl,match}) => (
             history.push(`${linkUrl}`)
         }} 
     >
-    <img src = '../../../assets/advance-adult-skipping-rope-red.jpg'/>
+    {/* <img src = '../../../assets/advance-adult-skipping-rope-red.jpg'/> */}
     <div className = "content"> 
         <h1 className="title">{title}</h1>
         <span className = "subtitle">SHOP NOW</span>
